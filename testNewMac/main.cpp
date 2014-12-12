328678b7dd0b2e5d3db315aede2678570ecd1bae
Lei Shi <larry.leishi@gmail.com>
2014-12-12 10:16:35 -0800

    Initial Commit

---
+++ testNewMac/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  testNewMac
+//
+//  Created by leshi on 12/12/14.
+//  Copyright (c) 2014 leshi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
